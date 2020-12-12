4d4d3e75d00613383b92d0eea8f88f272783e821
pawan panth <pawan.panth.pp@gmail.com>
2020-12-12 12:16:12 +0530

    m

--- src/index.js
+++ src/index.js
@@ -57,6 +57,7 @@ app.get("/newsFeeds", async (req,res)=>{
         ]);
         res.status(200).send(docs);
    }
+   res.send();
 })
 
 app.listen(port, () => console.log(`App listening on port ${port}!`))
